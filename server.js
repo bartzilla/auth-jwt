3441233d0033918074f6244b322a46090b34d0a7
Cipriano Sanchez <ciprianosanchez@gmail.com>
2017-01-06 14:15:03 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,94 @@
+// Include our packages in our main server file
+var express = require('express');
+app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var passport = require('passport');
+var config = require('./config/main');
+var User = require('./app/models/user');
+var jwt = require('jsonwebtoken');
+var port = 3000;
+
+// Use body-parser to get POST requests for API use
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Log requests to console
+app.use(morgan('dev'));
+
+// Initialize passport for use
+
+app.use(passport.initialize());
+
+// Connect to db
+mongoose.connect(config.database);
+
+// Bring in passport strategy we just defined
+require('./config/passport')(passport);
+
+// Create API group routes
+var apiRoutes = express.Router();
+
+// Register new users
+apiRoutes.post('/register', function (req, res) {
+  if(!req.body.email || !req.body.password){
+    res.json({success: false, message: 'Please enter an email address to register.'})
+  }else{
+    var newUser = new User({
+      email: req.body.email,
+      password: req.body.password
+    });
+
+    // Attempt to save the new user
+    newUser.save(function (err) {
+      if(err) return res.json({success: false, message: 'That email address already exists.'});
+
+      res.json({success: true, message: 'Successfully created new user.'})
+    });
+  }
+});
+
+// Authenticate the user and get a JSON Web Token to include in the header of future requests.
+apiRoutes.post('/authenticate', function(req, res) {
+  User.findOne({
+    email: req.body.email
+  }, function(err, user) {
+    if (err) throw err;
+
+    if (!user) {
+      res.send({ success: false, message: 'Authentication failed. User not found.' });
+    } else {
+      // Check if password matches
+      user.comparePassword(req.body.password, function(err, isMatch) {
+        if (isMatch && !err) {
+          // Create token if the password matched and no error was thrown
+          var token = jwt.sign(user, config.secret, {
+            expiresIn: 10080 // in seconds
+          });
+          res.json({ success: true, token: 'JWT ' + token });
+        } else {
+          res.send({ success: false, message: 'Authentication failed. Passwords did not match.' });
+        }
+      });
+    }
+  });
+});
+
+// Protect dashboard route with JWT
+apiRoutes.get('/dashboard', passport.authenticate('jwt', { session: false }), function(req, res) {
+  res.send('It worked! User id is: ' + req.user._id + '.');
+});
+
+
+// Set url for API group routes
+app.use('/api', apiRoutes);
+
+// Home route
+app.get('/', function(req, res){
+  res.send('Relax, we will put the home page here later.');
+});
+
+
+app.listen(port);
+console.log('Your server is running on port: ' + port + '.');
